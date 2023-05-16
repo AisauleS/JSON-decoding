1304e90cc984bcaac0253adba0af899bf9ea213c
AisauleS <aisaulesibatova@gmail.com>
2023-05-16 22:12:05 +0500

    Json decoding

--- JSON decoding/ViewController.swift
+++ JSON decoding/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let firstUrl = createUrl(with: "Black%Lotus")
+        let secondUrl = createUrl(with: "OPT")
+        callApi(url: firstUrl)
+        callApi(url: secondUrl)
     }
+    
+    
+    private func createUrl(with queryItemName: String) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.magicthegathering.io"
+        components.path = "/v1/cards"
+        components.queryItems = [URLQueryItem(name: "name", value: queryItemName)]
+        return components.url
+    }
+    
+    private func callApi(url: URL?) {
+        guard let url = url else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+        
+            if let error = error { print(error) }
+            
+            guard let response = response as? HTTPURLResponse else { return }
+            
+            guard let data = data else { return }
+            
+            print("Status code: \(response.statusCode)")
 
-
+            do {
+                let data = try JSONDecoder().decode(Cards.self, from: data)
+                guard let card = data.cards.first else {return}
+                print("Json data: \(card)")
+            }
+            catch {
+                print("Error: \(error)")
+            }
+        }.resume()
+    }
 }
-
